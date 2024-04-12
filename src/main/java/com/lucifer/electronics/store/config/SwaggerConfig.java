7d44ad17244d9b2d194b89ea452847a1a5e9405c
IamVk009 <imvk009@gmail.com>
2024-04-12 17:08:13 +0530

    Approach - 1: Implementing Spring Security in Swagger

--- src/main/java/com/lucifer/electronics/store/config/SwaggerConfig.java
+++ src/main/java/com/lucifer/electronics/store/config/SwaggerConfig.java
@@ -1,19 +1,41 @@
 package com.lucifer.electronics.store.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    String schemeName = "bearerScheme";
+
+    /**
+     * This method configures an OpenAPI object with security schemes and metadata.
+     * @return Configured OpenAPI object
+     */
     @Bean
     public OpenAPI openAPI(){
+//      Way - 1: Implementing Spring Security in Swagger
+//      Creating and configuring an OpenAPI object.
         return new OpenAPI()
+//              Adding security of type defined in below component to each and every API endpoint.
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(schemeName))
+//              Adding a security scheme of type HTTP with bearer token authentication to the components.
+                .components(new Components()
+                        .addSecuritySchemes(schemeName, new SecurityScheme()
+                                .name(schemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .bearerFormat("JWT")
+                                .scheme("bearer")))
+//              Setting the metadata information for the API, including title, description, version, contact information, license information and external documentation for an API.
                 .info(new Info().title("TheElectronicsStore API")
                         .description("This is TheElectronicsStore project API developed By VK")
                         .version("1.0")
